e9f67a4828385174c659244bdc06bf572dd39744
ningxiaoxiao <732869825@qq.com>
2017-10-26 13:28:52 +0800

    完成

--- shudu6/Program.cs
+++ shudu6/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
-using System.Threading;
 
 namespace shudu6
 {
@@ -9,227 +9,264 @@ namespace shudu6
     {
         static void Main(string[] args)
         {
-            var s = new sudoku();
+            if (args == null) throw new ArgumentNullException(nameof(args));
+
+            var sw = new Stopwatch();
+            sw.Start();
+
+            var s = new Sudoku();
 
             /*
             //no7
-            s.add(0, "005042");
-            s.add(1, "000013");
-            s.add(2, "006000");
-            s.add(3, "000100");
-            s.add(4, "630000");
-            s.add(5, "250400");
+            s.Add(0, "005042");
+            s.Add(1, "000013");
+            s.Add(2, "006000");
+            s.Add(3, "000100");
+            s.Add(4, "630000");
+            s.Add(5, "250400");
             */
 
             /*
             //no8
-            s.add(0, "000000");
-            s.add(1, "064250");
-            s.add(2, "500004");
-            s.add(3, "300006");
-            s.add(4, "051430");
-            s.add(5, "000000");*/
+            s.Add(0, "000000");
+            s.Add(1, "064250");
+            s.Add(2, "500004");
+            s.Add(3, "300006");
+            s.Add(4, "051430");
+            s.Add(5, "000000");
+            */
 
 
             // no9
-            s.add(0, "043000");
-            s.add(1, "000500");
-            s.add(2, "010600");
-            s.add(3, "004020");
-            s.add(4, "001000");
-            s.add(5, "000130");
+            s.Add(0, "043000");
+            s.Add(1, "000500");
+            s.Add(2, "010600");
+            s.Add(3, "004020");
+            s.Add(4, "001000");
+            s.Add(5, "000130");
 
             /*
             //no10
-            s.add(0, "020006");
-            s.add(1, "400000");
-            s.add(2, "001600");
-            s.add(3, "002400");
-            s.add(4, "000005");
-            s.add(5, "500060");
+            s.Add(0, "020006");
+            s.Add(1, "400000");
+            s.Add(2, "001600");
+            s.Add(3, "002400");
+            s.Add(4, "000005");
+            s.Add(5, "500060");
             */
             /*
             //no11
-            s.add(0, "102000");
-            s.add(1, "000020");
-            s.add(2, "010360");
-            s.add(3, "035010");
-            s.add(4, "020000");
-            s.add(5, "000402");
+            s.Add(0, "102000");
+            s.Add(1, "000020");
+            s.Add(2, "010360");
+            s.Add(3, "035010");
+            s.Add(4, "020000");
+            s.Add(5, "000402");
             */
 
 
-            Console.WriteLine(s.output);
-            s.solution();
+            Console.WriteLine("原始问题:\r\n" + s);
+            s.Solution();
+            sw.Stop();
+            Console.WriteLine($"共花费时间[{sw.ElapsedMilliseconds}]ms 计算[{Sudoku.SolutionCount}]次 找到解为:\r\n{Sudoku.SolutionSring}");
             Console.ReadKey();
         }
     }
-    public class sudoku
+    public class Sudoku
     {
-
-        public string output
+        public static int SolutionCount;
+        public static string SolutionSring;
+        public static object Locker { get; } = new object();
+        private static bool _findout;
+        private int _deth;
+        private int[,] _map = new int[6, 6];
+        private string _description = "原始";
+        private bool _noSolution;
+        private bool Complete
         {
             get
             {
-                var ret = "";
-                for (int r = 0; r < 6; r++)
+                for (var r = 0; r < 6; r++)
                 {
-                    for (int c = 0; c < 6; c++)
+                    for (var c = 0; c < 6; c++)
                     {
-                        ret += map[r, c] + " ";
-                    }
-                    ret += "\r\n";
-                }
-                return ret;
-            }
-        }
-
-        public bool complete
-        {
-            get
-            {
-                for (int r = 0; r < 6; r++)
-                {
-                    for (int c = 0; c < 6; c++)
-                    {
-                        if (map[r, c] == 0)
+                        if (_map[r, c] == 0)
                             return false;
                     }
                 }
                 return true;
             }
         }
-
-        public int count = 0;
-        public int sovercount = 0;
-        public int[,] map = new int[6, 6];
-
-        public string durtion = "原始";
-
-        public void solution()
+        private string Output
         {
-
-            for (int r = 0; r < 6; r++)
+            get
             {
-                for (int i = 0; i < 6; i++)
+                var ret = "┌───┬───┐\r\n";
+                for (var r = 0; r < 6; r++)
                 {
-                    if (findsolution)
+                    ret += "│ ";
+                    for (var c = 0; c < 6; c++)
                     {
-                        Console.WriteLine(Thread.CurrentThread.ManagedThreadId+"已经找到解,退出子线程");
-                        return;
+                        ret += _map[r, c] + (c == 2 ? "│" : " ");
                     }
-                    Cross(r, i);
+
+                    ret += "│\r\n";
+                    if (r == 1 || r == 3) ret += "├───┼───┤\r\n";
+                }
+
+                return ret + "└───┴───┘\r\n";
+            }
+        }
+        public void Add(int r, string s)
+        {
+            var nums = Sting2Nums(s);
+            for (var i = 0; i < 6; i++)
+            {
+                _map[r, i] = nums[i];
+            }
+        }
+        public void Solution()
+        {
+            Log("开始解");
+            SolutionCount++;
+            bool onealldone;
+            var findonedeth = 0;
+            do
+            {
+                onealldone = Findone();
+                findonedeth++;
+            } while (!onealldone);
+            Log($"一层解共循环{findonedeth}次");
+            if (_noSolution)
+                return;
+
+            if (Complete)
+            {
+                _findout = true;
+                SolutionSring = Output;
+
+                Log("找到解", ConsoleColor.Red);
+                return;
+            }
+
+            Findtwo();
+        }
+        /// <summary>
+        /// 找出所有惟一解, 找不到 返回真
+        /// </summary>
+        /// <returns></returns>
+        private bool Findone()
+        {
+            var onecount = 0;
+            for (var r = 0; r < 6; r++)
+            {
+                for (var i = 0; i < 6; i++)
+                {
+                    var s = GetCrossPossibility(r, i);
+                    if (s == null) continue;
+
+                    if (s.Length == 0)
+                    {
+                        Log("无解");
+                        _noSolution = true;
+                        return true;
+                    }
+                    if (s.Length != 1) continue;
+                    _map[r, i] = s[0];
+                    onecount++;
                 }
             }
-
-            sovercount++;
-
-            if (complete)
+            return onecount == 0;
+        }
+        private void Findtwo()
+        {
+            for (var r = 0; r < 6; r++)
             {
-                findsolution = true;
-                //找到解
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} 找到解:\r\n" + output);
-                Console.ForegroundColor = ConsoleColor.White;
+                for (var i = 0; i < 6; i++)
+                {
+                    if (_findout)
+                    {
+                        Log("已经找到解");
+                        return;
+                    }
+                    var s = GetCrossPossibility(r, i);
+                    if (s?.Length != 2) continue;
+                    foreach (var num in s)
+                    {
+                        //分出两个版本 尝试进行解
+                        var one = new Sudoku
+                        {
+                            _deth = _deth++,
+                            _map = (int[,])_map.Clone()
+                        };
+                        one._map[r, i] = num;
+                        one._description = $"[{r},{i}] 处 [{num}] 可能性";
+                        one.Solution();
+                        if (_findout)
+                            return;
+
+
+                    }
+                }
             }
         }
-
-        private static bool findsolution = false;
-        public void add(int r, string s)
+        private static int[] Sting2Nums(string s)
         {
-            int[] nums = splitnum(s);
-            for (int i = 0; i < 6; i++)
-            {
-                map[r, i] = nums[i];
-            }
-        }
-
-        public override string ToString()
-        {
-            return output;
-        }
-
-        public int[] splitnum(string s)
-        {
-            if (s.Length != 6) new Exception("应该是6个数字");
-            int[] ret = new int[6];
-
-
-            var count = 0;
+            if (s.Length != 6) throw new Exception("应该是6个数字");
+            var ret = new int[6];
+            
+            var i = 0;
             foreach (var mumstr in s)
             {
-                ret[count] = int.Parse(mumstr.ToString());
-                count++;
+                ret[i] = int.Parse(mumstr.ToString());
+                i++;
             }
+
             return ret;
         }
-        public void Cross(int r, int c)
+        private int[] GetCrossPossibility(int r, int c)
         {
             //计算总共是不是有5个数字
-            if (map[r, c] != 0)
-                return;
-            var rowmissnums = getRowMiss(r);
-            var colnums = getCol(c);
-            var box = getBox(r, c);
+            if (_map[r, c] != 0)
+                return null;
+            var rowMissd = GetRowMiss(r);
+            var col = GetCol(c);
+            var box = GetBox(r, c);
 
-            foreach (var i in colnums)
+            foreach (var i in col)
             {
-                rowmissnums.Remove(i);
+                rowMissd.Remove(i);
             }
 
             foreach (var i in box)
             {
-                rowmissnums.Remove(i);
+                rowMissd.Remove(i);
             }
 
-            if (rowmissnums.Count == 1)
-            {
-                //惟一解,写上map
-                map[r, c] = rowmissnums[0];
-                return;
-            }
-            if (rowmissnums.Count == 0)
-            {
-                //无解
-                Console.WriteLine($"{count} ,[{r},{c}] 出现0解");
-                Thread.CurrentThread.Abort();
-                return;
-            }
-
-            if (rowmissnums.Count == 2)
-            {
-                var ss = "";
-                foreach (var rstring in rowmissnums)
-                {
-                    ss += rstring + " ";
-                }
-                Console.WriteLine($"{count}的[{r},{c}]出现{rowmissnums.Count}个解[ {ss}],分裂");
-                foreach (var num in rowmissnums)
-                {
-                    
-                    lock (locker)
-                    {
-                        //分出两个版本 尝试进行解
-                        var one = new sudoku
-                        {
-                            count = count++,
-                            map = (int[,])map.Clone()
-                        };
-                        one.map[r, c] = num;
-                        one.durtion = $"[{r},{c}] 处 [{num}]可能性";
-
-                        var t = new Thread(one.solution);
-                        t.Start();
-                    }
-                }
-            }
-
-
+            return rowMissd.ToArray();
         }
-        static object locker=new object();
-        public List<int> getBox(int r, int c)
+        private List<int> GetRowMiss(int r)
         {
-            var box = new List<int>();
+            var rows = GetRow(r);
+            var miss = new List<int>();
+            for (var i = 1; i < 7; i++)
+            {
+                if (!rows.Contains(i)) miss.Add(i);
+            }
+            return miss;
+        }
+        private int[] GetRow(int r)
+        {
+            var ret = new int[6];
+            for (var i = 0; i < 6; i++)
+            {
+                ret[i] = _map[r, i];
+            }
+            return ret;
+        }
+        private int[] GetBox(int r, int c)
+        {
+            var box = new int[6];
             var nextrow = -1;
             switch (r)
             {
@@ -259,71 +296,37 @@ namespace shudu6
             }
 
             var startCol = c > 2 ? 3 : 0;
-            for (int i = 0; i < 3; i++)
+            for (var i = 0; i < 3; i++)
             {
-                box.Add(map[r, startCol + i]);
-                box.Add(map[nextrow, startCol + i]);
+                box[i] = (_map[r, startCol + i]);
+                box[i + 3] = (_map[nextrow, startCol + i]);
             }
             return box;
         }
-
-        public List<int> getRowMiss(int r)
+        private int[] GetCol(int c)
         {
-            var rows = getRow(r);
-            var miss = new List<int>();
-            for (int i = 1; i < 7; i++)
+            var ret = new int[6];
+            for (var i = 0; i < 6; i++)
             {
-                if (!rows.Contains(i)) miss.Add(i);
-            }
-            return miss;
-        }
-
-        public List<int> getRow(int r)
-        {
-            var ret = new List<int>();
-            for (int i = 0; i < 6; i++)
-            {
-                ret.Add(map[r, i]);
+                ret[i] = _map[i, c];
             }
             return ret;
         }
-
-        public List<int> getCol(int c)
+        private void Log(string msg)
         {
-            var ret = new List<int>();
-            for (int i = 0; i < 6; i++)
-            {
-                ret.Add(map[i, c]);
-            }
-            return ret;
+            Console.WriteLine($"深度[{_deth}] {_description}:{msg}");
         }
+        private void Log(string msg, ConsoleColor c)
+        {
+            var orgin = Console.ForegroundColor;
 
+            Console.ForegroundColor = c;
+            Log(msg);
+            Console.ForegroundColor = orgin;
+        }
+        public override string ToString()
+        {
+            return Output;
+        }
     }
-
-    public class box
-    {
-        int[,] map = new int[2, 3];
-    }
-
-    public class cell
-    {
-
-
-    }
-
-
-    public class col
-    {
-        int[] nums = new int[6];
-        public List<int> have = new List<int>();
-
-    }
-
-    public class row
-    {
-        int[] nums = new int[6];
-    }
-
-
-
 }
